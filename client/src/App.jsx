3f4d6026a2813f9acd4c9dd9575452639c293310
wengshang <wengshang.hoo@sophicautomation.com>
2023-10-17 15:31:13 +0800

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
+import UpdateListing from './pages/UpdateListing'
+import Search from './pages/Search'
 
 export default function App() {
   return <BrowserRouter>
@@ -14,7 +19,16 @@ export default function App() {
     <Route path="/sign-in" element={<Signin/>}/>
     <Route path="/sign-up" element={<SignUp/>}/>
     <Route path="/about" element={<About/>}/>
-    <Route path="/profile" element={<Profile/>}/>
+    <Route path='/listing/:listingId' element={<Listing />} />
+    <Route path='/search' element={<Search />} />
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+    </Route>
   </Routes>
   </BrowserRouter>
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBFVAOd4-YNxNFHqSFkT-LeV8RLlafSeI",
+  authDomain: "mern-estate-d2505.firebaseapp.com",
+  projectId: "mern-estate-d2505",
+  storageBucket: "mern-estate-d2505.appspot.com",
+  messagingSenderId: "1099086625305",
+  appId: "1:1099086625305:web:312c940ebd700dd04e0888"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
